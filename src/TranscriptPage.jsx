c5ac348cbd4f102a0d723d7161b81f4e79c92310
a0ms1nn <162542840+a0ms1nn@users.noreply.github.com>
2025-02-04 03:07:16 +0700

    edit 2 page

--- src/TranscriptPage.jsx
+++ src/TranscriptPage.jsx
@@ -16,26 +16,30 @@ export default function TranscriptPage() {
             </nav>
 
             {/* Transcript Section */}
-            <div className="containerTranscript">
-                <img src="/Transcript.JPG" alt="Transcript" />
-                <p>Dwload My Transcript</p>
-                <a 
-                    href="https://drive.google.com/file/d/1otdG-oY9uxU1-5lZjDgjlPfeE3G8Tyri/view" 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                    className="download-button"
-                >
-                    Download Transcript
-                </a>
+            <div className="resume-section">
+            <div className="resume-container">
+                <img src="/Transcript.JPG" alt="Resume" className="resume-image" />
+                <div className="resume-download">
+                    <p>Download my Transcript</p>
+                    <a 
+                        href="https://drive.google.com/file/d/1otdG-oY9uxU1-5lZjDgjlPfeE3G8Tyri/view?usp=drive_link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="download-btn"
+                    >
+                        📄 Download transcript
+                    </a>
+                </div>
             </div>
+        </div>
 
-            {/* Footer */}
-            <div className="footer">
-                <h2>Contact</h2>
-                <p>Email: <a href="mailto:satitkunrus_t@silpakorn.edu">satitkunrus_t@silpakorn.edu</a></p>
-                <p>Phone: <a href="tel:0988993238">098-899-3238</a></p>
-                <p>Address: Minburi, Bangkok, Thailand</p>
-            </div>
+        {/* Footer */}
+        <div className="footer">
+            <h2>Contact</h2>
+            <p>Email: satitkunrus_t@silpakorn.edu</p>
+            <p>Phone: 098-899-3238</p>
+            <p>Address: Minburi, Bangkok, Thailand</p>
+        </div>
         </div>
     );
 }
